896bc414f459351e6e2c2f49ec930a0ebc0949cb
sadie100 <03sadie@gmail.com>
2024-10-04 11:03:41 +0900

    웹팩에서의 플래그 타입 찍어 봄

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 console.log("웹팩에서의 플래그 찍어 봄...");
 console.log(process.env.FLAG);
+console.log(typeof process.env.FLAG);
 
 module.exports = (env) => {
   const entry = process.env.FLAG ? "./src/yesFlag.js" : "./src/noFlag.js";
