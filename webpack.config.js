70d5c5cab7416b8e9a36ae534bc48f137e0e32b8
sadie100 <03sadie@gmail.com>
2024-10-04 11:01:24 +0900

    웹팩에서 플래그 찍어 봄

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 
+console.log("웹팩에서의 플래그 찍어 봄...");
+console.log(process.env.FLAG);
+
 module.exports = (env) => {
   const entry = process.env.FLAG ? "./src/yesFlag.js" : "./src/noFlag.js";
   return {
