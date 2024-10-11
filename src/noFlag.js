6db1b9e2154e03b06fd283ac4f8d256613d66c55
sadie100 <03sadie@gmail.com>
2024-10-11 16:22:46 +0900

    set default setting : get cypress-realworld-app-develop
    https://github.com/cypress-io/cypress-realworld-app

--- src/noFlag.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-
-console.log("테스트가 작동합니당");
-console.log("플래그가 없습니당.");
-console.log(process.env.FLAG);
--- src/yesFlag.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-
-console.log("테스트가 작동합니당");
-console.log("플래그가 있습니당🎉");
-console.log(process.env.FLAG);
