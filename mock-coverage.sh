da8880b6a7f05e77e0cd9ae37623c7e1a5692ee5
sadie100 <03sadie@gmail.com>
2024-10-11 11:40:25 +0900

    로그 및 아티팩트 주소를 받아서 PR 커밋으로 올리는 액션 테스트

---
+++ mock-coverage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "=============================== Coverage summary ==============================="
+echo "Statements   : hooray~"
+echo "Branches     : yeah~"
+echo "Functions    : awesome~"
+echo "Lines        : Have a nice day"
+echo "================================================================================"
